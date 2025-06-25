a658368200266008bbcc51de72a8d14c6bb59944
Josue Byiringiro <josue.byiringiro@skrey-software.com>
2025-06-25 19:10:51 +0200

    Updated jobs model

--- src/database/models/jobs.ts
+++ src/database/models/jobs.ts
@@ -1,12 +1,18 @@
 import mongoose, { Schema } from 'mongoose';
-interface JobInterface { job_title: string; job_description: string; attachments: string[]; }
+interface JobInterface { job_title: string; job_description: string; attachments: string[]; positions_left: number; interviewed: number; applications: number; rejections: number; pending_feedback: number; offers: number; }
 
 const JobSchema = new Schema<JobInterface>(
   {
-    job_title: { type: String, required: true },
-    job_description: { type: String, required: true },
-    attachments: { type: [String], required: false, default: []}
-  },
+  job_title:         { type: String },
+  job_description:   { type: String },
+  positions_left:    { type: Number },
+  interviewed:       { type: Number, default: 0 },
+  applications:      { type: Number, default: 0 },
+  rejections:        { type: Number, default: 0 },
+  pending_feedback:  { type: Number, default: 0 },
+  offers:            { type: Number, default: 0 },
+  attachments:       { type: [String], default: [] },
+},
   { timestamps: true }
 );
 
